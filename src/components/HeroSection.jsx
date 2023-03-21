23de5527c36e93d5397bae7e78eed32337774d7f
angeloPradella-github <angelogpradella@gmail.com>
2023-03-21 15:17:26 +0100

    darkmode senza salvataggio funzionante

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,10 +4,10 @@ export default function HeroSection() {
       <h4 className="ff-accent clr-accent mb-5">Ciao, sono</h4>
       <h1 className="fw-h-bold">Angelo G. Pradella</h1>
       <p className="max-w-lg">
-        I’m a software engineer specializing in building (and occasionally
-        designing) exceptional digital experiences. Currently, I’m focused on
-        building accessible, human-centered products at
-        <span className="clr-accent">Upstatement</span>.
+        Perito informatico con competenze
+        <span className="clr-accent"> FullStack</span> di sviluppo Web,
+        appassionato per la progettazione di interfacce ed esperienze digitali
+        uniche.
       </p>
       <div className="btn-container flex gap-3 mt-6">
         <a className="btn-big ff-accent btn-empty up-transition" href="#">
--- src/components/SwitchContainer.jsx
+++ src/components/SwitchContainer.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export default function SwitchContainer() {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setDarkMode(!isDarkMode);
+    if (!isDarkMode) {
+      document.documentElement.style.setProperty("--clr-neutral", "#cdcdcd");
+      document.documentElement.style.setProperty("--clr-light", "#292929");
+      document.documentElement.style.setProperty("--clr-dark", "#fffbfb");
+    } else {
+      document.documentElement.style.setProperty("--clr-neutral", "#444452");
+      document.documentElement.style.setProperty("--clr-light", "#fffbfb");
+      document.documentElement.style.setProperty("--clr-dark", "#111");
+    }
+  };
+
   return (
     <div className="switch-container flex flex-col clr-neutral justify-end gap-8">
       <div>
-        <input type="checkbox" id="switch" />
+        <input type="checkbox" id="switch" onChange={handleToggle} />
         <label htmlFor="switch" className="drop-shadow-md">
           Toggle
         </label>
