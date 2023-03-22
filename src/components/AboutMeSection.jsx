24fb64fdd1a880b35ff017b79f78040bbfbf7eab
angeloPradella-github <angelogpradella@gmail.com>
2023-03-22 22:45:38 +0100

    aggiornata section about me

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -6,16 +6,16 @@ export default function AboutMeSection() {
     <section className="about-me mx-auto max-w-[900px]">
       <div className="flex flex-wrap">
         {/* ------------Description colum------------ */}
-        <div className="w-full md:w-2/3 px-7">
+        <div className="w-full lg:w-2/3 lg:px-7">
           <h2 className="fs-h2 fw-h-bold clr-neutral h2-b-bottom">Profilo.</h2>
-          <p className="mt-3 text-justify md:text-left">
+          <p className="mt-3 text-justify lg:text-left">
             Dopo aver conseguito il diploma di Perito Informatico, ho
             approfondito il mio interesse per lo{" "}
             <span className="clr-accent">sviluppo web</span> attraverso un corso
             di formazione FullStack, ampliando le mie competenze sia in ambito
             front-end che back-end.
           </p>
-          <p className="mt-3 text-justify md:text-left">
+          <p className="mt-3 text-justify lg:text-left">
             Le mie principali competenze includono la programmazione orientata
             agli oggetti e lo sviluppo di applicazioni web dinamiche con
             l'utilizzo di diversi linguaggi, framework e tecnologie. Ho maturato
@@ -24,14 +24,14 @@ export default function AboutMeSection() {
             Client-Server tramite chiamate a{" "}
             <span className="clr-accent">Webservice</span>.
           </p>
-          <p className="mt-3 text-justify md:text-left">
+          <p className="mt-3 text-justify lg:text-left">
             Nel mio lavoro, ambisco a coniugare tecnica e creatività,
             valorizzando le mie competenze informatiche per dar vita a soluzioni
             web efficaci e piacevoli da utilizzare.
           </p>
         </div>
         {/* ------------Photo colum------------ */}
-        <div className="w-full md:w-1/3 p-7 flex justify-center">
+        <div className="w-full lg:w-1/3 mt-5 lg:mt-0 lg:p-7 flex justify-center">
           <img
             className="profile-pic"
             src={ProfilePic}
@@ -39,8 +39,8 @@ export default function AboutMeSection() {
           />
         </div>
         {/* ------------Extra info row------------ */}
-        <section className="mt-6 w-full px-7">
-          <div className="flex justify-between extra-info-wrapper px-4 py-2">
+        <section className="mt-6 w-full lg:px-7">
+          <div className="flex flex-wrap gap-3 justify-between extra-info-wrapper px-4 py-2">
             <div>
               <h4>Sede</h4>{" "}
               <p className="text-base clr-neutral fw-b-bold">Trieste, Italia</p>
