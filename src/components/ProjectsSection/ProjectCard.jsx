0fb59d79807af77e5be60d97bf8f7ef2b4939a68
angeloPradella-github <angelogpradella@gmail.com>
2023-03-27 12:23:45 +0200

    project card con effetti iniziati, punto stabile

--- src/components/ProjectsSection/ProjectCard.jsx
+++ src/components/ProjectsSection/ProjectCard.jsx
@@ -1,4 +1,7 @@
-const ProjectCard = ({
+import React, { useEffect, useRef } from "react";
+import VanillaTilt from "vanilla-tilt";
+
+function ProjectCard({
   id,
   title,
   description,
@@ -8,9 +11,21 @@ const ProjectCard = ({
   imgPath,
   repoLink,
   demoLink,
-}) => {
+}) {
+  const tiltRef = useRef(null);
+  const options = {
+    max: 1, // Disabilita l'effetto di inclinazione
+    glare: true,
+    "max-glare": 0.2, // Opacità massima del glare (1 = 100%, 0.5 = 50%)
+    "glare-prerender": false, // Se impostato a false, VanillaTilt crea gli elementi glare per te
+  };
+
+  useEffect(() => {
+    VanillaTilt.init(tiltRef.current, options);
+  }, [options]);
+
   return (
-    <article className="project-card w-full lg:w-[48%]">
+    <article ref={tiltRef} className="project-card w-full lg:w-[48%]">
       <img className="project-img" src={imgPath} alt="" />
       <div className="hidden-content flex justify-center flex-col px-5">
         <h3 className="clr-light-fix drop-shadow-md">{title}</h3>
@@ -24,6 +39,6 @@ const ProjectCard = ({
       </div>
     </article>
   );
-};
+}
 
 export default ProjectCard;
