c8f739679befb8d5c646839e2c7ddd1ae57260e3
angeloPradella-github <angelogpradella@gmail.com>
2023-03-28 17:34:38 +0200

    implememntata funzionalità di dark mode e light mode salvandola nel local storage

--- src/components/SwitchContainer.jsx
+++ src/components/SwitchContainer.jsx
@@ -1,41 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const themeStyles = {
+  normal: {
+    "--clr-neutral": "#cdcdcd",
+    "--clr-neutral-opaque": "#cdcdcd5b",
+    "--clr-light": "#292929",
+    "--clr-dark": "#e6e6e6",
+  },
+  dark: {
+    "--clr-neutral": "#444452",
+    "--clr-neutral-opaque": "#4444523d",
+    "--clr-light": "#fffbfb",
+    "--clr-dark": "#111",
+  },
+};
 
 export default function SwitchContainer() {
-  const [isDarkMode, setDarkMode] = useState(false);
+  const [currentTheme, setCurrentTheme] = useState(
+    localStorage.getItem("theme") || "normal"
+  );
+  // Questo useEffect viene eseguito ogni volta che cambia il valore di currentTheme (o quando montato la prima volta), così da poi richiamare la funzione setStyles per impostare il nuovo CSS
+  useEffect(() => {
+    setStyles();
+  }, [currentTheme]);
 
+  // Utilizzo Object.entries per avere un array chiave-valore dall'oggetto styles, per poi usare un forEach per impostare ogni proprietà di stile sull'elemento document.documentElement
+  const setStyles = () => {
+    const styles = themeStyles[currentTheme];
+    Object.entries(styles).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    });
+  };
+
+  //Switchiamo in base al tema corrente mettendo quello opposto
   const handleToggle = () => {
-    setDarkMode(!isDarkMode);
-    if (!isDarkMode) {
-      document.documentElement.style.setProperty("--clr-neutral", "#cdcdcd");
-      document.documentElement.style.setProperty(
-        "--clr-neutral-opaque",
-        "#cdcdcd5b"
-      );
-      document.documentElement.style.setProperty("--clr-light", "#292929");
-      document.documentElement.style.setProperty("--clr-dark", "#e6e6e6");
-    } else {
-      document.documentElement.style.setProperty("--clr-neutral", "#444452");
-      document.documentElement.style.setProperty(
-        "--clr-neutral-opaque",
-        "#4444523d"
-      );
-      document.documentElement.style.setProperty("--clr-light", "#fffbfb");
-      document.documentElement.style.setProperty("--clr-dark", "#111");
-    }
+    const newTheme = currentTheme === "normal" ? "dark" : "normal";
+    setCurrentTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
   };
 
   return (
     <div className="switch-container flex flex-col clr-neutral justify-end gap-8">
       <div
         className="c-tooltip relative"
-        onChange={handleToggle}
         aria-label="Cambia Tema"
         data-direction="right"
       >
-        <input type="checkbox" id="switch" />
+        <input
+          onChange={handleToggle}
+          type="checkbox"
+          id="switch"
+          checked={currentTheme !== "dark"} //Per far partire lo switch nella modaltà corrente
+        />
         <label htmlFor="switch">Toggle</label>
       </div>
-
       <div className="line"></div>
     </div>
   );
